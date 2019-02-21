a7627ce6467f282f87439643e97da2ecd2252dd9
Orow Chou <jou00088@hotmail.com>
2019-02-21 21:42:09 +0800

    add minus example

---
+++ JS-Practice/practice.js
@@ -0,0 +1,20 @@
+// option arguments 
+// skip var be skip
+function square(x, y) {
+    return y;
+}
+console.log(square(1, 2, 3));
+// → 2
+
+// minus
+function minus(a, b) {
+    if (b === undefined) return -a;
+    else return a - b;
+}
+
+console.log(minus(10));
+// → -10
+console.log(minus(10, 5));
+// → 5
+
+// function power()

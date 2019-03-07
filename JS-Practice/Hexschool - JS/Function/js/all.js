e4c2424230432bca3ef4dda29666231cbc15c083
Orow Chou <jou00088@hotmail.com>
2019-03-07 16:19:16 +0800

    alert in function calculator

--- JS-Practice/Hexschool - JS/Function/js/all.js
+++ JS-Practice/Hexschool - JS/Function/js/all.js
@@ -4,24 +4,41 @@
 //     document.getElementById("greetId").textContent = "三瓶水的價錢是" + total + "元";
 // }
 // count(1,2);
+// -----------------
+
+
 
 
 // -----全域變數(global)/區域變數(local)-----
 // 區域變數 - total
-
-function count(oneNum, TwoNum){
-    var total = (oneNum + TwoNum) * 10;
-    document.getElementById("greetId").textContent = "三瓶水的價錢是" + total + "元";
-}
-count(1,2);
-console.log(total); 
+// function count(oneNum, TwoNum){
+//     // 區域
+//     var total = (oneNum + TwoNum) * 10;
+//     document.getElementById("greetId").textContent = "三瓶水的價錢是" + total + "元";
+// }
+// count(1,2);
+// console.log(total); 
 // total => undefined
 
 // 全域變數 - total
-var total
-function count(oneNum, TwoNum){
-    total = (oneNum + TwoNum) * 10;
-    document.getElementById("greetId").textContent = "三瓶水的價錢是" + total + "元";
+// 全域
+// var total
+// function count(oneNum, TwoNum){
+//     // 區域
+//     total = (oneNum + TwoNum) * 10;
+//     document.getElementById("greetId").textContent = "三瓶水的價錢是" + total + "元";
+// }
+// count(1,2);
+// console.log(total);
+
+// ------------------------------------------
+
+
+// Function 計算機案例
+document.getElementById("countId").onclick = function (){
+    // alert('被點擊了')
+    var hamNum = document.getElementById("hamNumId").value;
+    var cokeNum = document.getElementById("cokeNumId").value;
+    alert(hamNum);
+    alert(cokeNum);
 }
-count(1,2);
-console.log(total);

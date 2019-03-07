669bff7dbbe09b0e850de5671272f6bbb5bddc60
Orow Chou <jou00088@hotmail.com>
2019-03-07 21:19:10 +0800

    total people check

--- JS-Practice/Hexschool - JS/Control-Judgement/js/all.js
+++ JS-Practice/Hexschool - JS/Control-Judgement/js/all.js
@@ -2,3 +2,8 @@ var myMonth = 3;
 var thisMonth = 3;
 var birthdayCheck = myMonth == thisMonth;
 document.getElementById('birthdayId').textContent = birthdayCheck;
+
+var currentPeople = 1;
+var totalPeople = 4;
+var peopleNothere = currentPeople != totalPeople ;
+document.getElementById('peopleId').textContent = peopleNothere;

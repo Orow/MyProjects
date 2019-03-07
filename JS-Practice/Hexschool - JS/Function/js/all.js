2a0b37f833a4902a1b575fb64afc9a6c1bfbb535
Orow Chou <jou00088@hotmail.com>
2019-03-07 14:20:01 +0800

    global & local variables

--- JS-Practice/Hexschool - JS/Function/js/all.js
+++ JS-Practice/Hexschool - JS/Function/js/all.js
@@ -1,6 +1,27 @@
-// 帶參數
+// -----帶參數-----
+// function count(oneNum, TwoNum){
+//     var total = (oneNum + TwoNum) * 10;
+//     document.getElementById("greetId").textContent = "三瓶水的價錢是" + total + "元";
+// }
+// count(1,2);
+
+
+// -----全域變數/區域變數-----
+// 區域變數 - total
+
 function count(oneNum, TwoNum){
     var total = (oneNum + TwoNum) * 10;
-    document.getElementById("greetId").textContent = "3瓶水的價錢是" + total + "元";
+    document.getElementById("greetId").textContent = "三瓶水的價錢是" + total + "元";
 }
 count(1,2);
+console.log(total); 
+// total => undefined
+
+// 全域變數 - total
+var total
+function count(oneNum, TwoNum){
+    total = (oneNum + TwoNum) * 10;
+    document.getElementById("greetId").textContent = "三瓶水的價錢是" + total + "元";
+}
+count(1,2);
+console.log(total);

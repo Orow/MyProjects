09fc5e45b18f03381bdeb31e4f7f61fb4d583ece
Orow Chou <jou00088@hotmail.com>
2019-03-07 21:52:37 +0800

    && - freebie

--- JS-Practice/Hexschool - JS/Control-Judgement/js/all.js
+++ JS-Practice/Hexschool - JS/Control-Judgement/js/all.js
@@ -1,9 +1,17 @@
-var myMonth = 3;
-var thisMonth = 3;
-var birthdayCheck = myMonth == thisMonth;
-document.getElementById('birthdayId').textContent = birthdayCheck;
+// ----折扣篇----
+// var myMonth = 3;
+// var thisMonth = 3;
+// var birthdayCheck = myMonth == thisMonth;
+// document.getElementById('birthdayId').textContent = birthdayCheck;
 
-var currentPeople = 1;
-var totalPeople = 4;
-var peopleNothere = currentPeople != totalPeople ;
-document.getElementById('peopleId').textContent = peopleNothere;
+// var currentPeople = 1;
+// var totalPeople = 4;
+// var peopleNothere = currentPeople != totalPeople ;
+// document.getElementById('peopleId').textContent = peopleNothere;
+
+
+// ----贈品篇----
+var myBill = 1580;
+var isVIP = false;
+var getFreebie = myBill > 1000 && isVIP == true;
+document.getElementById('andId').textContent = getFreebie;

17750472208a0211aa3fa75b8c99e431ff8b05e5
Orow Chou <jou00088@hotmail.com>
2019-03-09 00:13:47 +0800

    add querySelector

---
+++ JS-Practice/Hexschool - JS/DOM/js/all.js
@@ -0,0 +1,12 @@
+// querySelector
+
+// var el = document.getElementById('titleId')
+// el.textContent = '321123';
+
+// var el = document.querySelector('#titleId')
+// el.textContent = '2333';
+
+
+var el = document.querySelector('#titleId em')
+el.textContent = '2333444';
+

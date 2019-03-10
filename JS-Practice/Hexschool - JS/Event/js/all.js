67e6f661fba6cffb28826f8622a69c512f04382a
Orow Chou <jou00088@hotmail.com>
2019-03-10 13:27:49 +0800

    event bubbling & capturing

--- JS-Practice/Hexschool - JS/Event/js/all.js
+++ JS-Practice/Hexschool - JS/Event/js/all.js
@@ -14,18 +14,35 @@
 
 // ----onclick & addEventListener事件綁定的差異----
 // onclick只會顯示最後一個的事件
-var elOn = document.querySelector('.btnOn');
-elOn.onclick = function(){
-    alert('onclick-1');
-}
-elOn.onclick = function(){
-    alert('onclick-2');
-}
+// var elOn = document.querySelector('.btnOn');
+// elOn.onclick = function(){
+//     alert('onclick-1');
+// }
+// elOn.onclick = function(){
+//     alert('onclick-2');
+// }
 
-var elAdd = document.querySelector('.btnAdd');
-elAdd.addEventListener('click',function(){
-    alert('add-1');
-},false)
-elAdd.addEventListener('click',function(){
-    alert('add-2');
-},false)
+// addEventListener會兩個事件都觸發到
+// var elAdd = document.querySelector('.btnAdd');
+// elAdd.addEventListener('click',function(){
+//     alert('add-1');
+// },false)
+// elAdd.addEventListener('click',function(){
+//     alert('add-2');
+// },false)
+
+
+// ----Event Bubbling & Capturing----
+var elbox = document.querySelector('.box');
+elbox.addEventListener('click',function(){
+    alert('hello box');
+    console.log('box');
+},false);
+
+var elbody =document.querySelector('.body');
+elbody.addEventListener('click',function(){
+    alert('hello body');
+    console.log('body');
+},true);
+//false - 從指定元素往外找 - event bubbling
+//true - 從最外面往裡面找指定元素 - event capturing

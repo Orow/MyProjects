c00fe7f05d95843b87093fbd7f6a8b71cebe858c
Orow Chou <jou00088@hotmail.com>
2019-03-10 18:14:50 +0800

    dataset to get infos from 'data-'

--- JS-Practice/Hexschool - JS/LocalStorage/js/all.js
+++ JS-Practice/Hexschool - JS/LocalStorage/js/all.js
@@ -4,33 +4,61 @@
 
 // console.log(localStorage.getItem("myName"));
 
-var btn = document.querySelector('.btnClass');
-function saveName(e){
-    var str = document.querySelector('.textClass').value;
-    localStorage.setItem("myName",str);
-}
-btn.addEventListener('click',saveName);
+// var btn = document.querySelector('.btnClass');
+// function saveName(e){
+//     var str = document.querySelector('.textClass').value;
+//     localStorage.setItem("myName",str);
+// }
+// btn.addEventListener('click',saveName);
 
-var call =document.querySelector('.btnCall');
-function callName(e){
-    var str = localStorage.getItem('myName');
-    alert(str);
-}
-call.addEventListener('click',callName);
+// var call =document.querySelector('.btnCall');
+// function callName(e){
+//     var str = localStorage.getItem('myName');
+//     alert(str);
+// }
+// call.addEventListener('click',callName);
+
+
+
 
 
 // ---- array to string / string to array ----
 // 因為localstorage只會保存string資料
-var county = [
-    {farmer : '哈維'}
-];
+// var county = [
+//     {farmer : '哈維'}
+// ];
 
-var countyString = JSON.stringify(county)
-console.log(countyString);
-localStorage.setItem('countyItem',countyString);
+// var countyString = JSON.stringify(county)
+// console.log(countyString);
+// localStorage.setItem('countyItem',countyString);
 
-var getData = localStorage.getItem('countyItem');
-console.log(getData);
+// var getData = localStorage.getItem('countyItem');
+// console.log(getData);
 
-var getDataArray = JSON.parse(getData);
-console.log(getDataArray[0].farmer);
+// var getDataArray = JSON.parse(getData);
+// console.log(getDataArray[0].farmer);
+
+
+
+
+// ----data-set 抓取資料----
+var dataSet = document.querySelector('.listli').dataset;
+console.log(dataSet);
+
+var dataSetDetail1 = document.querySelector('.listli').dataset.num;
+console.log(dataSetDetail1);
+
+var dataSetDetail2 = document.querySelector('.listli').dataset.dog;
+console.log(dataSetDetail2);
+
+var listLi = document.querySelector('.listli');
+// 確認點擊的人名以及相關資訊
+
+function checkList(e){
+    var num = e.target.dataset.num;
+    var dog = e.target.dataset.dog;
+    console.log('第' + num + '個人');
+    console.log('有'+ dog +'隻狗');
+}
+
+listLi.addEventListener('click',checkList,false);

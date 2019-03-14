61b065f42e8338336ce03f5226f80a9559e255df
Orow Chou <jou00088@hotmail.com>
2019-03-14 13:19:43 +0800

    quiz JS - for lopp in empty question(eval())

--- ProjectsInJS&jQuery/SimpleJavascriptQuiz/js/all.js
+++ ProjectsInJS&jQuery/SimpleJavascriptQuiz/js/all.js
@@ -1,2 +1,21 @@
+function submitAnswers() {
+    var total = 5;
+    var scroe = 0;
 
+    // Get User Input
+    var q1 = document.forms["quizForm"]["q1"].value;
+    var q2 = document.forms["quizForm"]["q2"].value;
+    var q3 = document.forms["quizForm"]["q3"].value;
+    var q4 = document.forms["quizForm"]["q4"].value;
+    var q5 = document.forms["quizForm"]["q5"].value;
 
+    // Validation
+    for (var i = 1; i <= total; i++) {
+        // qi, q固定i是迴圈=>eval('q'+ i )
+        if (eval('q'+i) == null || eval('q'+i) == "") {
+            alert('You missed question ' + i);
+            return false;
+        }
+    }
+
+}

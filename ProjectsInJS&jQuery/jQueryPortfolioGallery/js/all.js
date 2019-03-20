51d5786822fe056355d1b32674a6534d0b198f98
Orow Chou <jou00088@hotmail.com>
2019-03-20 23:18:42 +0800

    done in categories in JS

---
+++ ProjectsInJS&jQuery/jQueryPortfolioGallery/js/all.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('nav a').on('click', function(){
+        // current class assignment
+        $('nav li.current').removeClass('current');
+        $(this).parent().addClass('current');
+
+        // Set Heading text
+        $('h1#heading').text($(this).text()); 
+
+        // Get & Filter link text
+        var category = $(this).text().toLowerCase().replace(' ','-');
+
+        // Remove hidden class if 'all-projects' is selected
+        if(category == 'all-projects'){
+            $('ul#gallery li:hidden').fadeIn('slow').removeClass('hidden');
+        }   else{
+            $('ul#gallery li').each(function(){
+                if(!$(this).hasClass(category)){
+                    $(this).hide().addClass('hidden');
+                }   else {
+                    $(this).fadeIn('slow').removeClass('hidden');
+                }
+            });
+        }
+        // Stop link behaviour
+        return false;
+    });
+});

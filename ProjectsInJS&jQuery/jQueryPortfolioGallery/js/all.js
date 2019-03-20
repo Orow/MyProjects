3006a0ebcd37395191e9d3273bd498ee26040dd3
Orow Chou <jou00088@hotmail.com>
2019-03-21 02:16:40 +0800

    add add overlay effect

--- ProjectsInJS&jQuery/jQueryPortfolioGallery/js/all.js
+++ ProjectsInJS&jQuery/jQueryPortfolioGallery/js/all.js
@@ -25,4 +25,41 @@ $(document).ready(function(){
         // Stop link behaviour
         return false;
     });
+    // Mouseenter overlay
+    $('ul#gallery li').on('mouseenter',function(){
+        // Get data attritube values
+        var title = $(this).children().data('title');
+        var desc = $(this).children().data('desc');
+
+        // Validation
+        if(desc == null){
+            desc = 'Click To Enlarge ';
+        }
+        if(title == null){
+            title = ''; 
+        }
+
+        // Create overlay div
+        $(this).append('<div class="overlay"></div>');
+        // Get the overlay div
+        var overlay = $(this).children('.overlay');
+
+        // Add html to overaly;
+        overlay.html('<h3>' +title+ '</h3><p>' +desc+ '</p>');
+
+        // Fade in overlay
+        overlay.fadeIn(800);
+    });
+
+    // Mouseleave overlay
+    $('ul#gallery li').on('mouseleave',function(){
+        // Create overlay div
+        $(this).append('<div class="overlay"></div>');
+        // Get the overlay div
+        var overlay = $(this).children('.overlay');
+
+        // Fade out overlay
+        overlay.fadeOut(500);
+
+    });
 });

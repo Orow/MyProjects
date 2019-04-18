7ca628c1bbccb3651a823488150b5c804eb4aa80
Orow Chou <jou00088@hotmail.com>
2019-04-18 23:11:05 +0800

    modified weather time

--- weather-cwb/js/all.js
+++ weather-cwb/js/all.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   const URI =
     "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-23657188-F80E-42A7-A592-8F0EB8D62E33";
-    
-    $(".select").change(function(e) {
+
+  $(".select").change(function(e) {
     // console.log(e);
     let select = e.target.value;
     // console.log(select);
@@ -18,22 +18,31 @@ $(document).ready(function() {
             value.weatherElement[0].time[0].parameter.parameterName;
           let weaDate = value.weatherElement[0].time[0].startTime;
           weaDate = weaDate.substring(0, 11);
-        //   console.log(weaDate);
+          let weaStartTime = value.weatherElement[0].time[0].startTime;
+          weaStartTime = weaStartTime.substring(11, 16);
+          let weaEndTime = value.weatherElement[0].time[0].endTime;
+          weaEndTime = weaEndTime.substring(11, 16);
           let weaRainPercent =
             value.weatherElement[1].time[0].parameter.parameterName + "%";
-          let weaMinT =
-            value.weatherElement[2].time[0].parameter.parameterName;
-          let weaMaxT =
-            value.weatherElement[4].time[0].parameter.parameterName;
+          let weaMinT = value.weatherElement[2].time[0].parameter.parameterName;
+          let weaMaxT = value.weatherElement[4].time[0].parameter.parameterName;
           let cityWeather =
             '<div class="weather-item"><div class="wea-title"><h3 class="city-name">' +
             value.locationName +
-            '</h3><span class="time">今日18:00 - 明日06:00</span></div><div class="wea-content"><p class="date">日期：'+weaDate+'</p><p class="wea-info">天氣現象：' +
+            '</h3><span class="time">' +
+            weaDate +
+            '</span></div><div class="wea-content"><p class="date">時間：' +
+            weaStartTime +
+            " - " +
+            weaEndTime +
+            '</p><p class="wea-info">天氣現象：' +
             weaStatus +
             '</p><p class="temperature">氣溫：' +
             weaMinT +
             " - " +
-            weaMaxT + "&deg" +"C"+
+            weaMaxT +
+            "&deg" +
+            "C" +
             '</p><p class="rainPercent">降雨機率：' +
             weaRainPercent +
             "</p></div></div>";
@@ -42,7 +51,6 @@ $(document).ready(function() {
             str += cityWeather;
           } else if (select == "all") {
             str += cityWeather;
-            // location[index].locationName
           } else if (select == "sixCity") {
             if (value.locationName == "臺北市") {
               str += cityWeather;
@@ -68,5 +76,5 @@ $(document).ready(function() {
       }
     });
   });
-  $(".select").val('all').trigger('change');
+  $(".select").val("all").trigger("change");
 });
